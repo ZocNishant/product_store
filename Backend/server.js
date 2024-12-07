8e3d5448e6228b20b78c8a70dba13db4d61656d8
Nishant Zoc <zocnishant@gmail.com>
2024-12-07 21:59:51 +0545

    basics

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,27 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 import { connectdb } from "./config/db.js"
+import Product from "./models/product.model.js"
 
 const app = express()
 
-app.get('/products', (req, res)=>{
-    
+app.post('/products', async (req, res)=>{
+    const product = req.body  // user will send this data
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success:false, message: "Please Provide all the fields" })
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+        res.status(201).json({success:true, data: newProduct})
+    }catch (error){
+        console.log(error.message)
+        res.status(500).json({success:false, message: "Server Error"})
+        
+    }
 })
 
 app.listen(3000, () => {
